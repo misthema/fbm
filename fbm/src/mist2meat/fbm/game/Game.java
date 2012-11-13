f7cafa8fcd337013f3d538be47a0e83c650f3b02
Ville <blacknex@gmail.com>
2012-11-13 20:02:29 +0200

    -Added Player moving

--- fbm/src/mist2meat/fbm/game/Game.java
+++ fbm/src/mist2meat/fbm/game/Game.java
@@ -1,13 +1,21 @@
 package mist2meat.fbm.game;
 
+import org.newdawn.slick.Color;
+
+import mist2meat.fbm.objects.game.Player;
+import mist2meat.mkglib.io.Font;
+
 public class Game {
 	//TODO: MapHandler and Player class
+	Font lol;
+	public Player player;
+	
 	public Game(){
 		init();
 	}
 	
 	public void init(){
-		
+		player = new Player();
 	}
 	
 	public void process(){
@@ -18,11 +26,14 @@ public class Game {
 	public void update() {
 		//MapUpdate
 		//HUD-Update
+		player.update();
 		
 	}
 	
 	public void draw() {
+		lol.drawString(50, 50, "lolol");
 		//MapDraw
 		//HUD-Draw
+		player.draw();
 	}
 }
--- fbm/src/mist2meat/fbm/objects/game/Player.java
+++ fbm/src/mist2meat/fbm/objects/game/Player.java
@@ -1,5 +1,12 @@
 package mist2meat.fbm.objects.game;
 
+import java.io.IOException;
+
+import mist2meat.mkglib.Graphics;
+import mist2meat.mkglib.graphics.Image;
+import mist2meat.mkglib.io.Input;
+import mist2meat.mkglib.io.Keys;
+
 public class Player extends ControlledObject{
 	
 	public Player(){
@@ -27,7 +34,14 @@ public class Player extends ControlledObject{
 		
 	}
 	public void init(){
-		
+		x = 0;
+		y = 0;
+		try {
+			sprite = new Image("res/lol.png",50,50);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	@Override
@@ -38,12 +52,28 @@ public class Player extends ControlledObject{
 
 	@Override
 	public void draw() {
-		// TODO Auto-generated method stub
-		
+		Graphics.drawImage(sprite, x, y, false);
 	}
 	
 	public void handleControls(){
+		int keys[] = Input.getKeysDown();
 		
+		for(int i : keys){
+			switch(i){
+			case Keys.KEY_W:
+				y--;
+				break;
+			case Keys.KEY_S:
+				y++;
+				break;
+			case Keys.KEY_A:
+				x--;
+				break;
+			case Keys.KEY_D:
+				x++;
+				break;
+			}
+		}
 	}
 
 }
